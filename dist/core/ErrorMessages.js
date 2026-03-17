e875f9d903a363435d8ac8bea2ea738bf900e4b2
IgorMitckevich <mickevich_elenaa@mail.ru>
2026-03-17 17:39:27 +0300

    1

--- dist/core/ErrorMessages.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorsMessages = void 0;
-const errorsMessages = (errors) => {
-    return { errorsMessages: errors };
-};
-exports.errorsMessages = errorsMessages;
